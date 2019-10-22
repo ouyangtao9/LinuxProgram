661fe84729a932fc427fb35c19a1e14143cfadba
ouy <729572102@qq.com>
2019-10-21 20:42:50 -0700

    test

---
+++ linuxprogram/week6/code/dynamiclib/lib.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <dlfcn.h>
+#define NUM 100
+int sum(int*, int);
+int max(int*, int);
+void cre_table(int*, int);
---
+++ linuxprogram/week6/code/dynamiclib/test3.c
@@ -0,0 +1,46 @@
+#include "lib.h"
+
+int main(){
+	int a[NUM];
+	void *handle;
+	void (*f1)();
+	int (*f2)(), (*f3)();
+	char *error;
+
+	handle = dlopen("/home/ouy/a1/week6/libdyna.so", RTLD_LAZY);
+
+	if(!handle){
+		fprintf(stderr, "%s\n", dlerror());
+		exit(1);
+	}
+
+	f1= dlsym(handle, "cre_table");
+	if((error=dlerror())!=NULL)
+	{
+		fprintf(stderr, "%s\n", dlerror());
+		exit(1);
+	}
+	f2= dlsym(handle, "max");
+	if((error=dlerror())!=NULL)
+	{
+		fprintf(stderr, "%s\n", dlerror());
+		exit(1);
+	}
+	
+	f3= dlsym(handle, "sum");
+	if((error=dlerror())!=NULL)
+	{
+		fprintf(stderr, "%s\n", dlerror());
+		exit(1);
+	}
+
+	
+	f1(a, NUM);
+	printf("MAX=%d\n", f2(a, NUM));
+	printf("SUM=%d\n", f3(a, NUM));
+	if(dlclose(handle)<0){
+		fprintf(stderr, "%s\n", dlerror());
+		exit(1);
+	}
+	return 0;
+}
